a1442bd4eded512df0f00e37fdc0d922fe750ac6
Guilty Code <Calebjcoffin@gmail.com>
2018-05-15 21:19:32 -0230

    dbh.inc.php
    changed $dbPassword to match Main Test Server

--- htdocs/loginsystem/includes/dbh.inc.php
+++ htdocs/loginsystem/includes/dbh.inc.php
@@ -2,7 +2,7 @@
 
 $dbServername = "localhost";
 $dbUsername = "root";
-$dbPassword = "werty123";
+$dbPassword = "werty";
 $dbName = "loginsystem";
 
 $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
