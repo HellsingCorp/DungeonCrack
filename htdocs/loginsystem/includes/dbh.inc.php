a49fe78ab52b479d822799d2e45a503c24db6dfb
Guilty Code <Calebjcoffin@gmail.com>
2018-05-15 21:17:05 -0230

    Login System
    -fixed up login script
    -added logout button and script

--- htdocs/loginsystem/includes/dbh.inc.php
+++ htdocs/loginsystem/includes/dbh.inc.php
@@ -2,7 +2,7 @@
 
 $dbServername = "localhost";
 $dbUsername = "root";
-$dbPassword = "";
+$dbPassword = "werty123";
 $dbName = "loginsystem";
 
 $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
