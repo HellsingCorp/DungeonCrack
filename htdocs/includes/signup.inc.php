d53b1ced453a8b7c1f0684ac024c82d401706706
Guilty Code <Calebjcoffin@gmail.com>
2018-05-20 06:00:13 -0230

    Merger to mainbranch
    final commit before merging over current work

--- htdocs/includes/signup.inc.php
+++ htdocs/includes/signup.inc.php
@@ -34,7 +34,8 @@ if (isset($_POST['submit'])) {
 					//Hashing the password
 					$hashedpwd = password_hash($pwd, PASSWORD_DEFAULT);
 					//Insert the user into the database
-					$sql = "INSERT INTO users (user_first, user_last, user_email, user_uid, user_pwd) VALUES ('$first', '$last', '$email', '$uid', '$hashedpwd');";
+					$sql = "INSERT INTO users (user_first, user_last, user_email, user_uid, user_pwd)
+                    VALUES ('$first', '$last', '$email', '$uid', '$hashedpwd');";
 					mysqli_query($conn, $sql);
 					header("Location: ../signup.php?signup=Success!");
 					exit();
--- htdocs/toon.php
+++ htdocs/toon.php
@@ -3,6 +3,7 @@
  * Created by GuiltyCode
  * Date: 5/20/2018
  */
+include_once 'toon.inc.php';
 session_start();
 ?>
 
@@ -11,14 +12,29 @@ session_start();
     <link href="style.css" rel="stylesheet" type="text/css"/>
 </head>
 
-
-<section class="main-container">
-
+<div class="nav-uname">
     <?php
-    if (isset($_SESSION['u_id'])) {
-        echo "toon info at some time soon!";
+    if (isset($_SESSION['u_id']))
+    {
+        echo "---Logged in to---";
+        echo $_SESSION['u_email'];
     }
+
+
+    $sql = "SELECT * FROM users WHERE user_uid='$uid'";
+    $result = $conn;
+    $resultCheck = mysqli_num_rows($result);
+    if ($resultCheck > 0) {
+    echo $_SESSION['u_character'];
+    exit();
+
+
+
+
+
+
     ?>
+</div>
 
 </section>
 
